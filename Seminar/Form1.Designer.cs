561d129340f4e77833c02454632fb3ee8a0a09d9
jfprgin <jprgin00@fesb.hr>
2022-11-16 23:08:47 +0100

    Second Commit

---
+++ Seminar/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace Seminar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.From = new System.Windows.Forms.Label();
+            this.listBoxFrom = new System.Windows.Forms.ListBox();
+            this.fromTb = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.toTb = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBoxTo = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.amountTb = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.save = new System.Windows.Forms.Button();
+            this.read = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // From
+            // 
+            this.From.AutoSize = true;
+            this.From.Location = new System.Drawing.Point(161, 48);
+            this.From.Name = "From";
+            this.From.Size = new System.Drawing.Size(33, 13);
+            this.From.TabIndex = 2;
+            this.From.Text = "From:";
+            // 
+            // listBoxFrom
+            // 
+            this.listBoxFrom.FormattingEnabled = true;
+            this.listBoxFrom.Items.AddRange(new object[] {
+            "HRK",
+            "USD",
+            "EUR",
+            "GBP"});
+            this.listBoxFrom.Location = new System.Drawing.Point(200, 32);
+            this.listBoxFrom.Name = "listBoxFrom";
+            this.listBoxFrom.Size = new System.Drawing.Size(59, 56);
+            this.listBoxFrom.TabIndex = 3;
+            this.listBoxFrom.SelectedIndexChanged += new System.EventHandler(this.listBoxFrom_SelectedIndexChanged);
+            // 
+            // fromTb
+            // 
+            this.fromTb.Location = new System.Drawing.Point(265, 45);
+            this.fromTb.Name = "fromTb";
+            this.fromTb.Size = new System.Drawing.Size(82, 20);
+            this.fromTb.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(265, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Custom:";
+            // 
+            // toTb
+            // 
+            this.toTb.Location = new System.Drawing.Point(481, 45);
+            this.toTb.Name = "toTb";
+            this.toTb.Size = new System.Drawing.Size(82, 20);
+            this.toTb.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(491, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Custom:";
+            // 
+            // listBoxTo
+            // 
+            this.listBoxTo.FormattingEnabled = true;
+            this.listBoxTo.Items.AddRange(new object[] {
+            "HRK",
+            "USD",
+            "EUR",
+            "GBP"});
+            this.listBoxTo.Location = new System.Drawing.Point(411, 32);
+            this.listBoxTo.Name = "listBoxTo";
+            this.listBoxTo.Size = new System.Drawing.Size(64, 56);
+            this.listBoxTo.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(382, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "To:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Amount:";
+            // 
+            // amountTb
+            // 
+            this.amountTb.Location = new System.Drawing.Point(73, 45);
+            this.amountTb.Name = "amountTb";
+            this.amountTb.Size = new System.Drawing.Size(82, 20);
+            this.amountTb.TabIndex = 1;
+            this.amountTb.TextChanged += new System.EventHandler(this.amountTb_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(588, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 26);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem1.Text = "toolStripMenuItem1";
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(24, 98);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(75, 23);
+            this.save.TabIndex = 14;
+            this.save.Text = "Save";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // read
+            // 
+            this.read.Location = new System.Drawing.Point(105, 98);
+            this.read.Name = "read";
+            this.read.Size = new System.Drawing.Size(75, 23);
+            this.read.TabIndex = 15;
+            this.read.Text = "Last input";
+            this.read.UseVisualStyleBackColor = true;
+            this.read.Click += new System.EventHandler(this.read_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.webBrowser1);
+            this.panel1.Location = new System.Drawing.Point(12, 136);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(663, 171);
+            this.panel1.TabIndex = 16;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.webBrowser1.Location = new System.Drawing.Point(0, -189);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(663, 360);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted_2);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.Control;
+            this.menuStrip1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.optionsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(687, 24);
+            this.menuStrip1.TabIndex = 18;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.resetToolStripMenuItem});
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.resetToolStripMenuItem.Text = "Reset";
+            this.resetToolStripMenuItem.Click += new System.EventHandler(this.resetToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(687, 322);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.read);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.toTb);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.listBoxTo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.fromTb);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBoxFrom);
+            this.Controls.Add(this.From);
+            this.Controls.Add(this.amountTb);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Currency converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label From;
+        private System.Windows.Forms.ListBox listBoxFrom;
+        private System.Windows.Forms.TextBox fromTb;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox toTb;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBoxTo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox amountTb;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Button read;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+    }
+}
+
---
+++ Seminar/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+namespace Seminar
+{
+    public partial class Form1 : Form
+    {
+        string saved = "";
+        int i = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBoxFrom_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string from, to;
+            if (fromTb.Text == "")
+            {
+                from = listBoxFrom.SelectedItem.ToString();
+            }
+            else
+                from = fromTb.Text;
+            if (toTb.Text == "")
+                to = listBoxTo.SelectedItem.ToString();
+            else
+                to = toTb.Text;
+            if (to == from)
+                MessageBox.Show("Same currency selected!", "Warning");
+            else
+                webBrowser1.ScriptErrorsSuppressed = true;
+                webBrowser1.Navigate("www.google.com/search?q=" + amountTb.Text + " " + from + " in " + to + "&oq=" + amountTb.Text + " " + from + " in " + to);
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void amountTb_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+        private void textBoxHistory_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter history = new StreamWriter("History.txt"))
+            {
+                string from, to;
+
+                if (fromTb.Text == "")
+                {
+                    from = listBoxFrom.SelectedItem.ToString();
+                }
+                else
+                    from = fromTb.Text;
+                if (toTb.Text == "")
+                    to = listBoxTo.SelectedItem.ToString();
+                else
+                    to = toTb.Text;
+                saved += amountTb.Text + " " + from + " " + to+ "\r\n";
+                history.WriteLine(saved);
+                history.Flush();
+                history.Close();
+                i++;
+            }
+        }
+
+        private void read_Click(object sender, EventArgs e)
+        {
+            string[] lines = File.ReadAllLines("History.txt");
+                string line = lines[i-2];
+                string number = string.Empty;
+                foreach (char c in line)
+                {
+                    number += c;
+                    if (c == ' ')
+                        break;
+                }
+                amountTb.Text = "";
+                amountTb.AppendText(number);
+        }
+
+        private void webBrowser1_DocumentCompleted_1(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void menuStrip2_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void resetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            amountTb.Text = "";
+            fromTb.Text = "";
+            toTb.Text = "";
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void webBrowser1_DocumentCompleted_2(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+    }
+}
